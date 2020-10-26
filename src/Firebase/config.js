721b0d4596d1423cfa7169198934a3c87359843f
Pius Lucky <luckypius50@gmail.com>
2020-10-26 06:46:21 +0100

    Update config.js

--- src/Firebase/config.js
+++ src/Firebase/config.js
@@ -2,13 +2,13 @@ import firebase from "firebase/app";
 import "firebase/auth";
 
 const firebaseConfig = firebase.initializeApp({
-  apiKey: "AIzaSyCs4Ljv2WG0khNP5Y4ROCQlFwa9MgP9gfE",
+  apiKey: "AIzaSyCs4Ljv2WG0khNP5Y4ROCQlFwa9XXXX",
   authDomain: "myauth-c3676.firebaseapp.com",
   databaseURL: "https://myauth-c3676.firebaseio.com",
   projectId: "myauth-c3676",
   storageBucket: "myauth-c3676.appspot.com",
   messagingSenderId: "1017446690789",
-  appId: "1:1017446690789:web:650e44a44f64a6c63d890c"
+  appId: "1:1017446690789:web:650e44a44f64a6c6XXXXX"
 });
 
 
